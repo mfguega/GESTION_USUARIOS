2e1f1a81d03a8355a7a910f9349c2879c1735e60
PaolaGuerrero1107 <jenny.guerrero@cumplimos.co>
2025-01-22 23:09:22 -0500

    Creacion Usuario Controller

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
+
+import java.util.List;
+import model.Usuario;
+import model.UsuarioDAO;
 
 /**
  *
  * @author pao_l
  */
 public class UsuarioController {
-    
+
+    private UsuarioDAO usuarioDAO;
+
+    public UsuarioController() {
+        this.usuarioDAO = new UsuarioDAO();
+    }
+
+    public void registrarUsuario(String nombre, String apellido, String username, String password, int id_rol) {
+        Usuario usuario = new Usuario(nombre, apellido, username, password, id_rol);
+        usuarioDAO.registrarUsuario(usuario);
+    }
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioDAO.listarUsuarios();
+    }
+
+    public void actualizarUsuario(String nombre, String apellido, String username, String password, int id_rol) {
+        Usuario usuario = new Usuario(nombre, apellido, username, password, id_rol);
+        usuarioDAO.actualizarUsuario(usuario);
+    }
+
+    public void eliminarUsuario(int id) {
+        usuarioDAO.eliminarUsuario(id);
+    }
 }
